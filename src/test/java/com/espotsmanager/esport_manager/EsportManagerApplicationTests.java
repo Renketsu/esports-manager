d5bde15cc529bab77d1ef27f0facd779f8d6dbff
Renketsu <renan.nanre3030@gmail.com>
2024-09-16 18:43:44 -0300

    First commit

---
+++ src/test/java/com/espotsmanager/esport_manager/EsportManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espotsmanager.esport_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
